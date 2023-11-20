fab066cb61fcf3eb1a5e93fb0401525112b5ac09
LEANDRO <LEANDRO@DESKTOP-630KO0H>
2023-11-19 23:11:11 -0300

    Adicionar arquivos de projeto.

---
+++ FirstProject/FirstProject/Data/SistemaTarefasDBContex.cs
@@ -0,0 +1,25 @@
+﻿using FirstProject.Data.Map;
+using FirstProject.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace FirstProject.Data
+{
+    public class SistemaTarefasDBContex : DbContext
+    {
+        public SistemaTarefasDBContex(DbContextOptions<SistemaTarefasDBContex> options)
+            : base(options) 
+        {
+        }
+
+        public DbSet<UsuarioModel> Usuarios { get; set; }
+        public DbSet<TarefaModel> Tarefa { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new UsuarioMap());
+            modelBuilder.ApplyConfiguration(new TarefaMap());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ FirstProject/FirstProject/Models/TarefaModel.cs
@@ -0,0 +1,13 @@
+﻿using FirstProject.Enums;
+
+namespace FirstProject.Models
+{
+    public class TarefaModel
+    {
+
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public string? Descricao { get; set; }
+        public StatusTarefa Status { get; set; }
+    }
+}
